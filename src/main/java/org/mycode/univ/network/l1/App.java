9285e210b1e0dff784de66f1659a5cd9bb899616
TergBug <tergbug@gmail.com>
2020-02-03 23:15:01 +0200

    Initial commit

--- src/main/java/org/mycode/univ/network/l1/App.java
+++ src/main/java/org/mycode/univ/network/l1/App.java
@@ -2,6 +2,7 @@ package org.mycode.univ.network.l1;
 
 public class App {
     public static void main( String[] args ) {
+        //sdfa
         for (int i = 1; i <= 20; i++) {
             System.out.println(FormulaFactory.getFormulaByVariant(i).calculate());
         }
