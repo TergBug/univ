3a4b63d734923b588fc98b7625a1183dd8ad19db
TergBug <tergbug@gmail.com>
2020-07-10 15:04:14 +0300

    AAAA

--- src/main/java/org/mycode/univ/network/l1/App.java
+++ src/main/java/org/mycode/univ/network/l1/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main( String[] args ) {
         for (int i = 1; i <= 20; i++) {
             System.out.println(FormulaFactory.getFormulaByVariant(i).calculate());
+            //dsakfdlfj;dkfj
         }
     }
 }
